6d8e562475045aed3b26ea5662d4468d91fe4b8e
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-31 19:12:09 +0530

    Initial Commit

---
+++ iExtension/iExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExtensionApp.swift
+//  iExtension
+//
+//  Created by Jigmet stanzin Dadul on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
