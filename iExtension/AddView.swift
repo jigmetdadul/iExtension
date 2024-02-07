f4ce4531fd37edb6a922aec89ed410d9c9b59c89
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-02-07 18:18:01 +0530

    final

---
+++ iExtension/AddView.swift
@@ -0,0 +1,52 @@
+//
+//  AddView.swift
+//  iExtension
+//
+//  Created by Jigmet stanzin Dadul on 06/02/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var name = String()
+    @State private var type = "Buisness"
+    @State private var amount = Double()
+    let types = ["Buisness", "Household"]
+    
+    var itemList:Expenses
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section("Enter the expense details"){
+                    TextField("Expense name", text: $name)
+                        .foregroundColor(.brown)
+                    Picker("Type of Expense", selection: $type) {
+                        ForEach(types, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    TextField("Expense Amount", value: $amount, format: .currency(code: "USD"))
+                        .foregroundColor(.brown)
+                }
+               
+            }
+            .navigationTitle("Add new Expenses")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                Button("Add"){
+                    let newItem = expensesItems(name: name, type: type, amount: amount)
+                    itemList.items.append(newItem)
+                    dismiss()
+                }
+                .foregroundColor(.black)
+            })
+        }
+    }
+}
+
+#Preview {
+    AddView(itemList: Expenses())
+}
--- iExtension/ContentView.swift
+++ iExtension/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 
-struct expensesItems{
+struct expensesItems: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -16,18 +17,39 @@ struct expensesItems{
 
 @Observable
 class Expenses{
-    var items = [expensesItems]()
+    var items = [expensesItems](){
+        didSet{
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+    init(){
+        if let savedItemLists = UserDefaults.standard.data(forKey: "items"), let decoded =  try? JSONDecoder().decode([expensesItems].self, from: savedItemLists){
+            items = decoded
+            return
+        }
+        items = []
+    }
 }
 
 struct ContentView: View {
     
     @State private var itemList = Expenses()
-    
+    @State private var showAddNewExpense = false
     var body: some View {
         NavigationStack{
             List{
-                ForEach(itemList.items, id: \.name){ items in
-                    Text(items.name)
+                ForEach(itemList.items){ items in
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text( items.name )
+                            Text(items.type)
+                        }
+                        Spacer()
+                        Text(items.amount, format: .currency(code: "USD"))
+                        
+                    }
                 }
                 .onDelete(perform: deleteItem)
             }
@@ -35,10 +57,12 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar(content: {
                 Button("Add item button", systemImage: "plus"){
-                    let newItem = expensesItems(name: "Text", type: "mode", amount: 10)
-                    itemList.items.append(newItem)
+                    showAddNewExpense = true
                 }
                 .foregroundColor(.black)
+            })
+            .sheet(isPresented: $showAddNewExpense, content: {
+                AddView(itemList: itemList)
             })
         }
     }
